c96ad3c6a67c59b3a2c3f193ab1d116753ae16c2
Imran Mentese <imran.mentese@arhs-spikeseed.com>
2020-08-08 13:47:52 +0200

    IM-0 - fix jest timezone

--- src/helpers/__tests__/index.spec.ts
+++ src/helpers/__tests__/index.spec.ts
@@ -2,6 +2,7 @@ import { getCurrentDateTime } from '../functions';
 
 describe('functions test suite', () => {
   it('check current date time function', () => {
-    expect(getCurrentDateTime(new Date('2019-06-25T11:00:31.983Z'))).toEqual('2019-6-25 13:0:31');
+    getCurrentDateTime();
+    expect(getCurrentDateTime(new Date('2019-06-25T13:00:31.983Z'))).toEqual('2019-6-25 13:0:31');
   });
 });
